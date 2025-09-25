858af422434e7d16844801d48fdd2b3b7ff4ea02
MihuNt3r <borovykmis@gmail.com>
2025-09-25 20:18:25 +0300

    feat: maximum depth of binary tree and same tree

---
+++ same_tree/src/main.rs
@@ -0,0 +1,43 @@
+// Definition for a binary tree node.
+use std::rc::Rc;
+use std::cell::RefCell;
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        match (p, q) {
+            (Some(p), Some(q)) => {
+                if p.borrow().val == q.borrow().val {
+                    Self::is_same_tree(p.borrow().left.clone(), q.borrow().left.clone()) &&
+                    Self::is_same_tree(p.borrow().right.clone(), q.borrow().right.clone())
+                } else {
+                    false
+                }
+            },
+            (None, None) => { true },
+            _ => false
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
